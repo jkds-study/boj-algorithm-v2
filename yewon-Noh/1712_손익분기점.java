1508f4e8b2dae43531695d0f708c4b31f288aeae
Ye Won Noh <80824750+yewon-Noh@users.noreply.github.com>
2023-02-08 23:49:41 +0900

    1712_손익분기점 #1

--- yewon-Noh/1712_손익분기점.java
+++ yewon-Noh/1712_손익분기점.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
-        int A = in.nextInt();  // 고정 비용
-        int B = in.nextInt();  // 가변 비용
-        int C = in.nextInt();  // 노트북 가격
+        int A = in.nextInt();
+        int B = in.nextInt();
+        int C = in.nextInt();
 
         if (C<=B) System.out.print("-1");
         else System.out.println(A/(C-B)+1);
